6e4da0b89d94f3785f1464dc94719b2f88412e34
divyatariwala <divutari555@gmail.com>
2026-03-23 20:44:04 +0000

    search bar fixed2

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -76,6 +76,7 @@ export default function DashboardPage() {
                      border-gray-300 dark:border-gray-600
                      focus:outline-none focus:ring-2 focus:ring-purple-500
                      transition-colors duration-500"
+          style={{zIndex:9999}}
         />
         <select
           value={selectedCity}
